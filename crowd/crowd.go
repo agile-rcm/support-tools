02d6e0fb9a64aebd83b605803a7a379dd24d802f
Franco Arendholz <franco.arendholz@agile-rcm.de>
2020-12-31 21:27:35 +0100

    Add crowd functionality

---
+++ crowd/crowd.go
@@ -0,0 +1,241 @@
+package crowd
+
+import (
+	"github.com/agile-rcm/crowd-go"
+)
+
+func AddUserToGroup(crowdUrl, crowdApp, crowdPassword, user, group string) error {
+
+	api, err := crowd.NewAPI(crowdUrl, crowdApp, crowdPassword)
+
+	if err != nil {
+		return err
+	}
+
+	err = api.AddUserToGroup(user, group)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func CreateGroup(crowdUrl, crowdApp, crowdPassword, groupName, groupDescription string, isActive bool) error {
+	api, err := crowd.NewAPI(crowdUrl, crowdApp, crowdPassword)
+
+	if err != nil {
+		return err
+	}
+
+	err = api.CreateGroup(groupName, groupDescription, isActive)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func RemoveGroup(crowdUrl, crowdApp, crowdPassword, groupName string) error {
+	api, err := crowd.NewAPI(crowdUrl, crowdApp, crowdPassword)
+
+	if err != nil {
+		return err
+	}
+
+	err = api.RemoveGroup(groupName)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func AddChildGroupMembership(crowdUrl, crowdApp, crowdPassword, parentGroupName, childGroupName string) error {
+	api, err := crowd.NewAPI(crowdUrl, crowdApp, crowdPassword)
+
+	if err != nil {
+		return err
+	}
+
+	err = api.AddChildGroupMembership(parentGroupName, childGroupName)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func AddParentGroupMembership(crowdUrl, crowdApp, crowdPassword, parentGroupName, childGroupName string) error {
+	api, err := crowd.NewAPI(crowdUrl, crowdApp, crowdPassword)
+
+	if err != nil {
+		return err
+	}
+
+	err = api.AddParentGroupMembership(parentGroupName, childGroupName)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func AddUser(crowdUrl, crowdApp, crowdPassword, userName, userPassword, userFirstName, userLastName, userDisplayName, userEmail string, isActive bool) error {
+
+	api, err := crowd.NewAPI(crowdUrl, crowdApp, crowdPassword)
+
+	if err != nil {
+		return err
+	}
+
+	err = api.AddUser(userName, userPassword, userFirstName, userLastName, userDisplayName, userEmail, isActive)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func GetUser(crowdUrl, crowdApp, crowdPassword, userName string) (*crowd.User, error) {
+
+	api, err := crowd.NewAPI(crowdUrl, crowdApp, crowdPassword)
+
+	if err != nil {
+		return nil, err
+	}
+
+	user, err := api.GetUser(userName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+
+}
+
+func UpdateUser(crowdUrl, crowdApp, crowdPassword, userName, userFirstName, userLastName, userDisplayName, userEmail string, isActive bool) error {
+
+	api, err := crowd.NewAPI(crowdUrl, crowdApp, crowdPassword)
+
+	if err != nil {
+		return err
+	}
+
+	err = api.UpdateUser(userName, userFirstName, userLastName, userDisplayName, userEmail, isActive)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func RemoveUser(crowdUrl, crowdApp, crowdPassword, userName string) error {
+	api, err := crowd.NewAPI(crowdUrl, crowdApp, crowdPassword)
+
+	if err != nil {
+		return err
+	}
+
+	err = api.RemoveUser(userName)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func GetUserAttributes(crowdUrl, crowdApp, crowdPassword, userName string) (*crowd.Attributes, error) {
+
+	api, err := crowd.NewAPI(crowdUrl, crowdApp, crowdPassword)
+
+	if err != nil {
+		return nil, err
+	}
+
+	attributes, err := api.GetUserAttributes(userName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return attributes, nil
+
+}
+
+func StoreUserAttribute(crowdUrl, crowdApp, crowdPassword, userName, attributeName, attributeValue string) error {
+
+	api, err := crowd.NewAPI(crowdUrl, crowdApp, crowdPassword)
+
+	if err != nil {
+		return err
+	}
+
+	attributes := crowd.Attributes{
+		Attributes: []*crowd.Attribute{
+			{
+				Name:   attributeName,
+				Values: []string{attributeValue},
+			},
+		},
+	}
+
+	err = api.StoreUserAttributes(userName, &attributes)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func RemoveUserAttribute(crowdUrl, crowdApp, crowdPassword, userName, attributeName string) error {
+	api, err := crowd.NewAPI(crowdUrl, crowdApp, crowdPassword)
+
+	if err != nil {
+		return err
+	}
+
+	err = api.RemoveUserAttribute(userName, attributeName)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func RemoveUserFromGroup(crowdUrl, crowdApp, crowdPassword, userName, groupName string) error {
+	api, err := crowd.NewAPI(crowdUrl, crowdApp, crowdPassword)
+
+	if err != nil {
+		return err
+	}
+
+	err = api.RemoveUserFromGroup(userName, groupName)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.15
 
 require (
 	github.com/360EntSecGroup-Skylar/excelize/v2 v2.3.1
+	github.com/agile-rcm/crowd-go v0.3.0
 	github.com/golang-migrate/migrate/v4 v4.13.0
 	github.com/hashicorp/vault/api v1.0.4
 	github.com/tealeg/xlsx v1.0.5
 	github.com/urfave/cli v1.22.4
+	github.com/valyala/fasthttp v1.18.0 // indirect
 	github.com/virtomize/confluence-go-api v1.2.5
 )
